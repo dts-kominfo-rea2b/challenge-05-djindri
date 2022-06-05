f4632dd7b7821e3c42295094e8ef9011109fe05e
djindri <indri.djon.hansemit@gmail.com>
2022-06-05 16:48:58 +0700

    sort ascending and descending

--- index.js
+++ index.js
@@ -6,15 +6,32 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, sortNames) => {
+  const list = [];
+  let display = sortNames(names);
+  
+  for (let x = 0; x < display.length; x++) {
+    y = x + 1 + "." + display[x];
+    list.push(y);
+  }
+
+  return list;
+}
+  
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  let sA = names.sort((x, y) => (x > y ? 1 : -1));
+  return sA;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => {
+  let sD = names.sort((x, y) => (x > y ? -1 : 1));
+  return sD;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
